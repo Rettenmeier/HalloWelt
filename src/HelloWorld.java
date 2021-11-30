192e86a9b2eea6a507568e0a032e3fac46d8bfd4
Rettenmeier <rettel01@thu.de>
2021-11-30 11:52:07 +0100

    neue Klasse erstellt

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int a=5;
+        int b=7;
+        int c=3;
+        int summe = a+b+c;
+        System.out.println(summe);
+        System.out.println("Hallo");
+    }
+}
